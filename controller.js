e20f90dd4f39b4681fad3ea4e7565677e2b09635
Andrew Carrigan <carrigan223@gmail.com>
2021-09-24 17:53:17 -0700

    added Controller handling basic CRUD funcionality for todo list

---
+++ controller.js
@@ -0,0 +1,83 @@
+/**
+ * Controller.js contains the controller class which will manage
+ * the functionality and logic of our apllication
+ */
+
+const { resolve } = require("path/posix");
+const data = require("./data.js");
+
+class Controller {
+    /**
+     * async function 'getTodos' will handle returning
+     * all todo objects
+     */
+    getTodos = async() => {
+        return new Promise((resolve, _) => resolve(data));
+    };
+
+    /**
+     * async function getTodos takes a single parameter 'id' to find
+     * and return a single todo object, if no todo found with matching
+     * id promise is rejected with message to user
+     */
+
+    getTodo = async(id) => {
+        return new Promise((resolve, reject) => {
+            let todo = data.find((id) => todo.id === parseInt(id));
+            if (todo) {
+                resolve(todo);
+            } else {
+                reject(`No Todos found with id ${id}.`);
+            }
+        });
+    };
+
+    /**
+     * async function createTodo is takeing a single parameter
+     * 'todoData' to which is appended with an id to generate
+     * a new todo object
+     */
+
+    creatTodo = async(todoData) => {
+        return new Promise((resolve, _) => {
+            let newTodo = {
+                id: Math.floor(4 + Math.random() * 10),
+                ...todoData
+            };
+            resolve(newTodo);
+        });
+    };
+
+    /**
+     * async function updateTodoStatus takes one parameter 'id'
+     * which then updates the completion status and returns the todo
+     */
+
+    updateTodoStatus = async(id) => {
+        return new Promise((resolve, reject) => {
+            let todo = data.find((todo) => todo.id === parseInt(id));
+            if (!todo) {
+                reject(`No todo found with id ${id} to be updated.`);
+            }
+            todo.completed = true;
+            resolve(todo);
+        });
+    };
+
+    /**
+     * async function deleteTodo will take a single parameter 'id'
+     * to locate a todo then remove it from the list of todos
+     */
+
+    deleteTodo = async(id) => {
+        return new Promise((resolve, reject) => {
+            let todo = data.find((todo) => todo.id === parseInt(id));
+            if (!todo) {
+                reject(`No todo found with id ${id} to be deleted.`);
+            }
+            resolve("Todo deleted successfully");
+        });
+    };
+}
+
+module.exports = Controller;
---
+++ data.js
@@ -0,0 +1,45 @@
+/*
+    Test data for API to send in response 
+*/
+const todos = [{
+    id: 1,
+    title: "Coding in Javascript",
+    description: "Working with functions in JavaScript",
+    completed: false,
+}, {
+    id: 2,
+    title: "Cooking Supper",
+    description: "Preparing rice and chicken",
+    completed: false,
+}, {
+    id: 3,
+    title: "Taking a walk",
+    description: "Easy time at the park",
+    completed: false,
+}, {
+    id: 4,
+    title: "Watching Netflix",
+    description: "Enjoying the new premiered series",
+    completed: false,
+}, {
+    id: 5,
+    title: "Coding in Javascript",
+    description: "Working with functions in JavaScript",
+    completed: false,
+}, {
+    id: 6,
+    title: "Cooking Supper",
+    description: "Preparing rice and chicken",
+    completed: false,
+}, {
+    id: 7,
+    title: "Taking a walk",
+    description: "Easy time at the park",
+    completed: false,
+}, {
+    id: 8,
+    title: "Watching Netflix",
+    description: "Enjoying the new premiered series",
+    completed: false,
+}, ];
+module.exports = todos;
